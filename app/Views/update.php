a25b262b907789130e279ec47cf9300daa0dcba4
Jesus Galindo <87211798+JesusGalindo20@users.noreply.github.com>
2021-09-16 21:13:04 -0300

    Update update.php
    Exibindo os dados a serem editados

--- app/Views/update.php
+++ app/Views/update.php
@@ -1,5 +1,6 @@
 <?php
 
+
 helper('form');
 
 echo form_open('bhaskara/frmInserir');
@@ -8,13 +9,13 @@ echo form_label('<br><br><br>--------------------------------- <br>', '');
 echo form_label('--------------------------------- <br>', '');
 echo form_label('MODIFICAR <br>', '');
 echo form_label('Digite o ID a modificar ', 'id');
-echo form_input('ID', '');
+echo form_input('ID', $olhar['ID']);
 echo form_label('Digite o valor de A ', 'valorA');
-echo form_input('A', '');
+echo form_input('A', $olhar['A']);
 echo form_label('Digite o valor de B ', 'valorB');
-echo form_input('B', '');
+echo form_input('B', $olhar['B']);
 echo form_label('Digite o valor de C ', 'valorC');
-echo form_input('C', '');
+echo form_input('C', $olhar['C']);
 echo form_submit('mysubmit', 'Modificar');
 echo form_close();
 
